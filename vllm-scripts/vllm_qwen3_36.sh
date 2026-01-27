c0cd57e982f5d955bb6f0abbe185451c35477f17
eric891224 <s96006730@gmail.com>
2026-01-27 13:25:04 +0800

    add qwen3 to 36 server

---
+++ vllm-scripts/vllm_qwen3_36.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# prevent OOM
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+deactivate
+source .venv/bin/activate
+which python
+
+set -x
+
+ip=$(hostname --ip-address)
+
+####################################################################################
+
+export CUDA_VISIBLE_DEVICES=0,1
+export TORCH_CUDA_ARCH_LIST="8.9" # for Ada Lovelace (RTX 4090, 4080, etc.)
+# export RAY_DEDUP_LOGS=0
+# export PYTHONUNBUFFERED=1
+debug=0
+
+
+cmd="vllm serve \
+    /mnt/data1/llm_team/Qwen3-4B-Thinking-2507 \
+    --tensor-parallel-size 2 \
+    --host 0.0.0.0 \
+    --port 8000 \
+    --max-model-len 128000 \
+    --gpu-memory-utilization 0.85 \
+    --reasoning-parser deepseek_r1"
+    
+bash -c "$cmd" > log 2>&1
