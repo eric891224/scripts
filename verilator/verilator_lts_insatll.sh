2015cdd15950043488d1d223e88dd13bfa61474a
eric891224 <s96006730@gmail.com>
2026-03-25 21:34:58 +0800

    update lts verilator installation script so that it install stable version

--- verilator/verilator_lts_insatll.sh
+++ verilator/verilator_lts_insatll.sh
@@ -1,9 +1,44 @@
+# ========== Required CLI Parameters ==========
+
+usage() {
+	echo "Usage: $0 --root_dir <path>"
+	exit 1
+}
+
+ROOT_DIR=""
+while [[ $# -gt 0 ]]; do
+	case "$1" in
+		--root_dir)
+			if [[ -z "$2" ]]; then
+				echo "Error: --root_dir requires a value."
+				usage
+			fi
+			ROOT_DIR="$2"
+			shift 2
+			;;
+		-h|--help)
+			usage
+			;;
+		*)
+			echo "Error: Unknown argument '$1'."
+			usage
+			;;
+	esac
+done
+
+if [[ -z "$ROOT_DIR" ]]; then
+	echo "Error: required parameter --root_dir is missing."
+	usage
+fi
+
+# ========== Installation for Verilator Stable Version ==========
+
 # uncomment if specific version is needed
 # VERILATOR_VERSION=v5.020
-PREFIX=$HOME/.usr
+PREFIX="$ROOT_DIR"
 
 pushd /tmp/
-git clone https://github.com/verilator/verilator.git
+git clone -b stable --depth 1 https://github.com/verilator/verilator.git
 cd verilator
 
 # uncomment if specific version is needed
@@ -19,7 +54,7 @@ autoconf                      # Create ./configure script
 make -j `nproc`
 make install
 popd
-rm -r /tmp/verilator
+rm -rf /tmp/verilator
 
 # Set the PATH environment variable
 export PATH=${PREFIX}/bin:$PATH
